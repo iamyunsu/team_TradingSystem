9ca6f709a73a58e53ab9cfe5048e33e27f4df73a
iamyunsu <iamyunsu@naver.com>
2024-05-21 14:04:43 +0900

    initial unittest

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,58 @@
+﻿#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-TEST(test, test) {
+TEST(testTradingSystem, 증권사_키워_로그인_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 증권사_네모_로그인_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 증권사_선택하지않고_로그인하면_실패) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 증권사_선택하고_로그인하지않고_buy하면_실패) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 증권사_선택하고_로그인하지않고_sell하면_실패) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 증권사_선택하고_로그인하지않고_getPrice하면_실패) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 키워에서_buy_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 네모에서_buy_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 키워에서_sell_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 네모에서_sell_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 키워에서_getPrice_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 네모에서_getPrice_성공) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 키워에서_buyNiceTiming했는데_기준에맞지않아서_예외) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(testTradingSystem, 네모에서_sellNiceTiming했는데_기준에맞지않아서_예외) {
 	EXPECT_EQ(1, 1);
 }
