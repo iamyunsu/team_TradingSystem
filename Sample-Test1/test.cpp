dc88ddd272b72f3d16500585ff1f307712f2627b
iamyunsu <iamyunsu@naver.com>
2024-05-21 15:02:06 +0900

    Add login test

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,12 +1,43 @@
-﻿#include "gmock/gmock.h"
+﻿#include <iostream>
+#include <string>
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "../team_TradingSystem/StockAdapter.cpp"
+
+using namespace testing;
 
 TEST(testTradingSystem, 증권사_키워_로그인_성공) {
-	EXPECT_EQ(1, 1);
+	KiwerAPI api;
+	string id = "AAA";
+	string pw = "BBB";
+
+	std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+	std::ostringstream strCout;
+	std::cout.rdbuf(strCout.rdbuf());
+
+	api.login(id, pw);
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string expected = id + " login success\n";
+	EXPECT_THAT(strCout.str(), testing::StrEq(expected));
 }
 
 TEST(testTradingSystem, 증권사_네모_로그인_성공) {
-	EXPECT_EQ(1, 1);
+	NemoAPI api;
+	string id = "AAA";
+	string pw = "BBB";
+
+	std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+	std::ostringstream strCout;
+	std::cout.rdbuf(strCout.rdbuf());
+
+	api.certification(id, pw);
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string expected = "[NEMO]" + id + " login GOOD\n";
+	EXPECT_THAT(strCout.str(), testing::StrEq(expected));
 }
 
 TEST(testTradingSystem, 증권사_선택하지않고_로그인하면_실패) {
