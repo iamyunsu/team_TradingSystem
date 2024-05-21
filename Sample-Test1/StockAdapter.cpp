78682befd1c06a07ae6f855520c0d4a5c51557ad
TaeHong Kim <artoflunar@gmail.com>
2024-05-21 13:57:09 +0900

    update StockAdapter skeleton code

---
+++ Sample-Test1/StockAdapter.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#
+using namespace std;
+
+class StockAdapter {
+	virtual void login(string id, string pass) = 0;
+	virtual void buy(int code, int price, int quantity) = 0;
+	virtual void sell(int code, int price, int quantity) = 0;
+	virtual int getPrice(int code) = 0;
+};
+
+class NemoStockAdapter : public StockAdapter {
+public:
+	void login(string id, string pass)
+	{
+
+	}
+	void buy(int code, int price, int quantity)
+	{
+
+	}
+	void sell(int code, int price, int quantity)
+	{
+
+	}
+	int getPrice(int code)
+	{
+
+	}
+};
+
+class KiwerStockAdapter : public StockAdapter {
+public:
+	void login(string id, string pass)
+	{
+
+	}
+	void buy(int code, int price, int quantity)
+	{
+
+	}
+	void sell(int code, int price, int quantity)
+	{
+
+	}
+	int getPrice(int code)
+	{
+
+	}
+};
