8e5e750b026b571619d004ed2ce2d76ed126884d
iamyunsu <iamyunsu@naver.com>
2024-05-21 15:12:25 +0900

    Add sell/buy test

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -57,19 +57,79 @@ TEST(testTradingSystem, 증권사_선택하고_로그인하지않고_getPrice하
 }
 
 TEST(testTradingSystem, 키워에서_buy_성공) {
-	EXPECT_EQ(1, 1);
+	KiwerAPI api;
+	string id = "AAA";
+	string pw = "BBB";
+
+	api.login(id, pw);
+
+	std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+	std::ostringstream strCout;
+	std::cout.rdbuf(strCout.rdbuf());
+
+	api.buy("SEC", 1, 20);
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string expected = "SEC : Buy stock ( 20 * 1)\n";
+	EXPECT_THAT(strCout.str(), StrEq(expected));
 }
 
 TEST(testTradingSystem, 네모에서_buy_성공) {
-	EXPECT_EQ(1, 1);
+	NemoAPI api;
+	string id = "AAA";
+	string pw = "BBB";
+
+	api.certification(id, pw);
+
+	std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+	std::ostringstream strCout;
+	std::cout.rdbuf(strCout.rdbuf());
+
+	api.purchasingStock("SEC", 1, 20);
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string expected = "[NEMO]SEC buy stock ( price : 1 ) * ( count : 20)\n";
+	EXPECT_THAT(strCout.str(), StrEq(expected));
 }
 
 TEST(testTradingSystem, 키워에서_sell_성공) {
-	EXPECT_EQ(1, 1);
+	KiwerAPI api;
+	string id = "AAA";
+	string pw = "BBB";
+
+	api.login(id, pw);
+
+	std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+	std::ostringstream strCout;
+	std::cout.rdbuf(strCout.rdbuf());
+
+	api.sell("SEC", 1, 20);
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string expected = "SEC : Sell stock ( 20 * 1)\n";
+	EXPECT_THAT(strCout.str(), StrEq(expected));
 }
 
 TEST(testTradingSystem, 네모에서_sell_성공) {
-	EXPECT_EQ(1, 1);
+	NemoAPI api;
+	string id = "AAA";
+	string pw = "BBB";
+
+	api.certification(id, pw);
+
+	std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
+	std::ostringstream strCout;
+	std::cout.rdbuf(strCout.rdbuf());
+
+	api.sellingStock("SEC", 1, 20);
+
+	std::cout.rdbuf(oldCoutStreamBuf);
+
+	string expected = "[NEMO]SEC sell stock ( price : 1 ) * ( count : 20)\n";
+	EXPECT_THAT(strCout.str(), StrEq(expected));
 }
 
 TEST(testTradingSystem, 키워에서_getPrice_성공) {
