36daac13a94f3b030d40c5aa3ad7c7799399ae67
ykwryu <ykw.ryu@samsung.com>
2024-05-21 14:46:12 +0900

    update select

---
+++ team_TradingSystem/AutoTradingSystem.cpp
@@ -0,0 +1,22 @@
+﻿#include <memory>
+#include <string>
+#include "StockAdapter.cpp"
+
+class StockerBrocker {
+public:
+	virtual void selectStockBrocker() = 0;
+	virtual void login() = 0;
+	virtual void buy() = 0;
+	virtual void sell() = 0;
+	virtual void getPrice() = 0;
+};
+
+class AutoTradingSystem {
+public:
+	void selectStockBrocker(StockAdapter* stockAdapter) {
+		stockBroker = stockAdapter;
+	}
+
+private:
+	StockAdapter* stockBroker;
+};
