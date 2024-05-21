c9350747c619f91183ebdbcc0a11e9058f404e40
ykwryu <ykw.ryu@samsung.com>
2024-05-21 14:56:10 +0900

    add api

--- team_TradingSystem/AutoTradingSystem.cpp
+++ team_TradingSystem/AutoTradingSystem.cpp
@@ -7,6 +7,18 @@ public:
 	void selectStockBrocker(StockAdapter* stockAdapter) {
 		stockBroker = stockAdapter;
 	}
+	void login(string id, string pass) {
+		stockBroker->login(id, pass);
+	}
+	void buy(int code, int price, int quantity) {
+		stockBroker->buy(code, price, quantity);
+	}
+	void sell(int code, int price, int quantity) {
+		stockBroker->sell(code, price, quantity);
+	}
+	int getPrice(int code) {
+		stockBroker->getPrice(code);
+	}
 
 private:
 	StockAdapter* stockBroker;
