451edee265265729f5ce15d795f0b6c0e1d9f84f
ykwryu <ykw.ryu@samsung.com>
2024-05-21 14:47:41 +0900

    remove

--- team_TradingSystem/AutoTradingSystem.cpp
+++ team_TradingSystem/AutoTradingSystem.cpp
@@ -2,15 +2,6 @@
 #include <string>
 #include "StockAdapter.cpp"
 
-class StockerBrocker {
-public:
-	virtual void selectStockBrocker() = 0;
-	virtual void login() = 0;
-	virtual void buy() = 0;
-	virtual void sell() = 0;
-	virtual void getPrice() = 0;
-};
-
 class AutoTradingSystem {
 public:
 	void selectStockBrocker(StockAdapter* stockAdapter) {
